f27184c15fef84ae7aef1079b0330d118e411dc9
Jawad Ali <alijamil734@gmail.com>
2021-03-30 23:28:36 +0500

    fix: tasks icon bug

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,46 +1,41 @@
 <template>
   <div>
-    <v-list v-if="tasks.length > 0" flat class="pt-0">
-      <div v-for="task in tasks" :key="task.id">
-        <v-list-item
-          @click="doneTask(task.id)"
-          :class="{ 'blue lighten-5': task.done }"
-        >
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-            </v-list-item-action>
+    <div v-if="tasks.length > 0">
+      <v-list flat class="pt-0">
+        <div v-for="task in tasks" :key="task.id">
+          <v-list-item
+            @click="doneTask(task.id)"
+            :class="{ 'blue lighten-5': task.done }"
+          >
+            <template v-slot:default>
+              <v-list-item-action>
+                <v-checkbox
+                  :input-value="task.done"
+                  color="primary"
+                ></v-checkbox>
+              </v-list-item-action>
 
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-                >{{ task.title }}</v-list-item-title
-              >
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-menu offset-y>
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn icon v-bind="attrs" v-on="on">
-                    <v-icon color="primary">mdi-dots-vertical</v-icon>
-                  </v-btn>
-                </template>
-                <v-list>
-                  <v-list-item>
-                    <v-list-item-content>
-                      <v-btn block flat>
-                        <v-icon left>{{ item.icon }}</v-icon>
-                        {{ item.title }}</v-btn
-                      >
-                    </v-list-item-content >
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-    </v-list>
+              <v-list-item-content>
+                <v-list-item-title
+                  :class="{ 'text-decoration-line-through': task.done }"
+                  >{{ task.title }}</v-list-item-title
+                >
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-menu offset-y>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon v-bind="attrs" v-on="on">
+                      <v-icon color="primary">mdi-dots-vertical</v-icon>
+                    </v-btn>
+                  </template>
+                </v-menu>
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+          <v-divider></v-divider>
+        </div>
+      </v-list>
+    </div>
     <div v-else>
       <v-container fill-height fluid>
         <v-row align="center" justify="center">
