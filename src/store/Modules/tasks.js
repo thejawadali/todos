43acc385aed1dde2ea47cbf5ee03e3db1578b884
Jawad Ali <alijamil734@gmail.com>
2021-04-02 22:24:56 +0500

    delete task

--- src/store/Modules/tasks.js
+++ src/store/Modules/tasks.js
@@ -3,7 +3,21 @@ export default {
   namespaced: true,
   state: {
     tasks: [
-      
+      {
+        id: "aabd0fa1-a064-4045-a8f6-016e17e68f18",
+        title: "Task 1",
+        done: false,
+      },
+      {
+        id: "bc75af2c-ebec-4a38-86ae-7fc1117cd0f5",
+        title: "Task 2",
+        done: false,
+      },
+      {
+        id: "6a2ca066-96e6-4f4e-90f5-731f7251a3b6",
+        title: "Task 3",
+        done: false,
+      },
     ],
   },
   mutations: {
@@ -18,6 +32,9 @@ export default {
         done: false,
       };
       state.tasks.push(newTask);
+    },
+    deleteTask(state, taskId) {
+      state.tasks = state.tasks.filter((task) => task.id.toString() !== taskId);
     },
   },
 };
